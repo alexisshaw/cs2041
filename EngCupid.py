a874efa9550ac531a7e44a3181be685085b28278
Alexis Shaw <alexis.shaw@gmail.com>
2012-05-25 22:56:58 +1000

    (10:39 - 10:41) Rename File

--- EngCupid.py
+++ EngCupid.py
@@ -2,8 +2,9 @@
 
 __author__ = 'WS02admin'
 
-print "Content-Type: text/html"     # HTML is following
-print                               # blank line, end of headers
+import genHTML
+
+print genHTML.getHTTPHeader()
 
 print "<TITLE>CGI script output</TITLE>"
 print "<H1>This is my first CGI script</H1>"
---
+++ genHTML.py
@@ -0,0 +1,8 @@
+__author__ = 'WS02admin'
+
+def getHTTPHeader():
+    header ="""
+Content-Type: text/html
+
+"""
+    return header;
