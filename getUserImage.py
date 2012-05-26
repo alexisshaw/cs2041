9ae01293bf0e250e88bfe4090a0eb0754342ed5c
ashaw <hvfdq234>
2012-05-27 00:59:56 +1000

    (12:13 - 12:16) Close my connections

--- getUserImage.py
+++ getUserImage.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python
+import connectToDatabase
 
+def getUserImage(environ, start_response):
+    connection = connectToDatabase.connect();
+
+    c = connection.cursor();
+    c.execute("SELECT image from table where userid = %s", )
