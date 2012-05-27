77477d041efc4be08a5ebb83af9428692a0b7ba7
ashaw <hvfdq234>
2012-05-28 00:33:14 +1000

    (23:30 - 23:45) Add page number query string

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+import re
 import wsgiref.handlers
+import notFound
 import ok
 from printUsers import getUserProfiles, countUserProfiles
 
@@ -14,15 +16,23 @@ def printAlphabetChooser():
 
 
 def printPage(environ, start_response):
+    m = re.search('pagenum=([^&]*)', environ['QUERY_STRING'])
+    pagenum = int(m.group(1) if m != None else 0)
+
+    responseCode = ok.code()
+
     string = ''
-    string += genHTML.genPageHeader("EngCupid")
     string += genHTML.genMenuBar("EngCupid", [dict(link='EngCupid.py', name='Home', active=True)])
     string += genHTML.beginContainer()
     string += str(countUserProfiles('a', '', '%'))
-    string += getUserProfiles('a', '', '%', 10, 0)
+    if pagenum > (countUserProfiles('a', '', '%')-1)/10:
+        responseCode = notFound.code()
+        string += """<div class='span12'><div class="hero-unit"><H1>404 :(</H1><p>Sorry but that page seems not to exist</p></div></div>"""
+    else:
+        string += getUserProfiles('a', '', '%', 10, pagenum)
     string += genHTML.endContainer()
     string += genHTML.genPageFooter()
-    start_response(ok.code(), [('Content-type', 'text/html')])
+    start_response(responseCode, [('Content-type', 'text/html')])
     return string
 
 wsgiref.handlers.CGIHandler().run(printPage)
