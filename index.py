c663e6defdf9ea009dd8d9a8a6844ecae2454a16
ashaw <hvfdq234>
2012-05-27 17:21:55 +1000

    (16:48 - 17:15) Debugging change (Fixed SQL Inject Token)

--- index.py
+++ index.py
@@ -21,19 +21,19 @@ c.execute("SELECT * FROM USERS ORDER BY userid DESC LIMIT 1")
 l = c.fetchone()
 internalCursor = conn.cursor()
 internalCursor.execute("SELECT operating_system FROM user_operating_systems " +
-                       "WHERE user_operating_systems.userid = ? "+
+                       "WHERE user_operating_systems.userid = %s "+
                        "ORDER BY operating_system ASC", [l['userid']])
 l['user_operating_systems'] = internalCursor.fetchall()
 internalCursor.execute("SELECT operating_system FROM user_operating_systems_wanted " +
-                       "WHERE user_operating_systems_wanted.userid = ? " +
+                       "WHERE user_operating_systems_wanted.userid = %s " +
                        "ORDER BY operating_system ASC", [l['userid']])
 l['user_operating_systems_wanted'] = internalCursor.fetchall()
 internalCursor.execute("SELECT programming_language FROM user_programming_languages " +
-                       "WHERE user_programming_languages.userid = ? " +
+                       "WHERE user_programming_languages.userid = %s " +
                        "ORDER BY programming_language ASC", [l['userid']])
 l['programming_language'] = internalCursor.fetchall()
 internalCursor.execute("SELECT programming_language FROM user_programming_languages_wanted " +
-                       "WHERE user_programming_languages_wanted.userid = ? " +
+                       "WHERE user_programming_languages_wanted.userid = %s " +
                        "ORDER BY programming_language ASC", [l['userid']])
 l['user_programming_languages_wanted'] = internalCursor.fetchall()
 internalCursor.close()
