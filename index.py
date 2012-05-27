73f3a22c59ea0da452d94b12cee70bac8056a40d
ashaw <hvfdq234>
2012-05-27 17:13:05 +1000

    (16:16 - 16:48) Do supplemental Queries for printing operating Systems and programming Languages

--- index.py
+++ index.py
@@ -19,6 +19,24 @@ conn = connectToDatabase.connect(dictCon = True)
 c    = conn.cursor()
 c.execute("SELECT * FROM USERS ORDER BY userid DESC LIMIT 1")
 l = c.fetchone()
+internalCursor = conn.cursor()
+internalCursor.execute("SELECT operating_system FROM user_operating_systems " +
+                       "WHERE user_operating_systems.userid = ? "+
+                       "ORDER BY user_operating_systems_wanted.operating_system ASC", [l['userid']])
+l['user_operating_systems'] = internalCursor.fetchall()
+internalCursor.execute("SELECT operating_system FROM user_operating_systems_wanted " +
+                       "WHERE user_operating_systems_wanted.userid = ? " +
+                       "ORDER BY ser_operating_systems_wanted.operating_system ASC", [l['userid']])
+l['user_operating_systems_wanted'] = internalCursor.fetchall()
+internalCursor.execute("SELECT operating_system FROM user_programming_languages " +
+                       "WHERE user_programming_languages.userid = ? " +
+                       "ORDER BY user_programming_languages.programming_language ASC", [l['userid']])
+l['programming_language'] = internalCursor.fetchall()
+internalCursor.execute("SELECT operating_system FROM user_programming_languages_wanted " +
+                       "WHERE user_programming_languages_wanted.userid = ? " +
+                       "ORDER BY user_programming_languages_wanted.programming_language ASC", [l['userid']])
+l['user_programming_languages_wanted'] = internalCursor.fetchall()
+internalCursor.close()
 c.close()
 conn.close()
 
